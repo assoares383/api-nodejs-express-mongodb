43a493457990c61b38f82d900b50b771a309be8d
assoares383 <alexandressoares383@gmail.com>
2020-12-16 18:50:53 -0300

    home registration

--- dev-code/backend/src/controllers/HouseController.js
+++ dev-code/backend/src/controllers/HouseController.js
@@ -1,6 +1,20 @@
+import House from "../models/House";
 class HouseController {
   async store(request, response) {
-    return response.json({ ok: true });
+    const { filename } = request.file;
+    const { description, price, location, status } = request.body;
+    const { user_id } = request.headers;
+
+    const house = await House.create({
+      user: user_id,
+      thumbnail: filename,
+      description,
+      price,
+      location,
+      status,
+    });
+
+    return response.json(house);
   }
 }
 
