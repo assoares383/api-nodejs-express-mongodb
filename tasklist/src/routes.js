6b8ed1a417c2e15a073c335fdcadf051bf84672d
assoares383 <alexandressoares383@gmail.com>
2020-12-20 17:44:04 -0300

    loading our model

--- tasklist/src/routes.js
+++ tasklist/src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from "express";
+import User from "./app/models/User";
 
 const routes = new Router();
 
-routes.get("/teste", (req, res) => res.json({ ok: true }));
+routes.get("/teste", async (req, res) => {
+  const user = await User.create({
+    name: "Alexandre",
+    email: "alexandressoares383@gmail.com",
+    password_hash: "123456",
+  });
+
+  res.json(user);
+});
 
 export default routes;
